8fdf48ca367d9a391ba09e2bc288ae067bdb38c3
2170chm <2170chm@gmail.com>
2024-04-01 23:59:59 -0700

    Finished initial implementation of user microservice

---
+++ src/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (user) => {
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+exports.register = async ({
+  username,
+  password,
+  firstName,
+  lastName,
+  roomNumber,
+}) => {
+  try {
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+      throw new Error("User already exists");
+    }
+    const newUser = new User({
+      username,
+      password,
+      firstName,
+      lastName,
+      roomNumber,
+      privilege: "resident",
+    });
+    await newUser.save();
+    return "User created successfully";
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+exports.login = async ({ username, password }) => {
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      throw new Error("Username is not registered");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new Error("Password incorrect");
+    }
+    const token = generateJWT(user);
+    return token;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
