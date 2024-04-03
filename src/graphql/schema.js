b645e267f5278d5e9382858df4137aa5ddaa6f45
2170chm <2170chm@gmail.com>
2024-04-03 03:26:13 -0700

    Fixed discrepancy of graphql schemas

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -7,13 +7,17 @@ const schema = buildSchema(`
   }
   type Mutation {
     register(username: String!, password: String!, firstName: String!, lastName: String!, roomNumber: Int!): String
-    login(username: String!, password: String!): String
+    login(username: String!, password: String!): AuthResponse
   }
   type User {
     username: String
     firstName: String
     lastName: String
     roomNumber: Int
+    privilege: String
+  }
+  type AuthResponse {
+    token: String
     privilege: String
   }
 `);
@@ -24,8 +28,16 @@ const root = {
   register: ({ username, password, firstName, lastName, roomNumber }) => {
     return register({ username, password, firstName, lastName, roomNumber });
   },
-  login: ({ username, password }) => {
-    return login({ username, password });
+  login: async ({ username, password }) => {
+    try {
+      const response = await login({ username, password });
+      return {
+        token: response.token,
+        privilege: response.privilege,
+      };
+    } catch (error) {
+      throw new Error(error.message);
+    }
   },
   getUser: async ({ username }) => {
     try {
